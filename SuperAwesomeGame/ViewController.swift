12d5c06839c17e44fc20b951af437b62507f9ce6
Brenden Vogt <bvogtdev@gmail.com>
2018-12-14 19:31:23 -0800

    Initial Commit

---
+++ SuperAwesomeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperAwesomeGame
+//
+//  Created by Brenden Vogt on 12/14/18.
+//  Copyright © 2018 BrendenVogt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
